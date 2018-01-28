c1d424b7f51797fee348a0c2ec31cdcebb701eb2
Marian Sigler <m@qjym.de>
2018-01-28 18:43:22 +0100

    add tables editor

--- extra/cleanup.sh
+++ extra/cleanup.sh
@@ -5,8 +5,9 @@ test -f extra/cleanup.sh &&
     grep '0Hth9zfK2C5Py8ofplNCa7VGGrIeeQ38tUsJysXBeXV9' extra/cleanup.sh -q ||
     { echo >&2 wrong directory.; exit 1; }
 
-rm -r src/sma/hm/classes
+rm -r src/sma/hm/classes src/sma/hm/tables
 rm -r bin
 rm gen/*
 rm metamodel/classes.gmf* metamodel/classes.genmodel metamodel/classes.trace
+rm metamodel/tables.gmf* metamodel/tables.genmodel metamodel/table.trace
 rm -r ../sma.hm.diagram ../sma.hm.edit ../sma.hm.editor ../sma.hm.tests

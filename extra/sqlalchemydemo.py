9ab836d571f0aa875877205770a28e90222cd3ba
Marian Sigler <m@qjym.de>
2018-01-30 23:40:29 +0100

    simplify sqlalchemy demo script

--- extra/sqlalchemydemo.py
+++ extra/sqlalchemydemo.py
@@ -8,55 +8,38 @@ sys.path.insert(0, '.')
 from gen.root import *
 from gen.root import Base
 
-def setup():
-    engine = create_engine('sqlite:///:memory:')#, echo=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    return session
 
-def do_demo1():
-    print('Using Database1.xmi')
-    u1 = Usuario(nombre='yo')
-    m1 = Mensaje(autor=u1, texto='hola')
-    print('u1 =', u1, u1.nombre)
-    print('m1 =', m1)
-    print(m1.autor == u1)
-    print('m1.autor =', m1.autor.nombre)
+print('==== Using Database1.xmi')
+u1 = Usuario(nombre='yo')
+m1 = Mensaje(autor=u1, texto='hola')
+print('u1 =', u1, u1.nombre)
+print('m1 =', m1)
+print(m1.autor == u1)
+print('m1.autor =', m1.autor.nombre)
 
 
-def do_demo2():
-    print('Using ClassDiagram1.xmi (transformed by the m2m class2table transformation)')
-    u1 = Usuario(nombre='yo')
-    f1 = Foro(nombre='Preguntas generales')
-    t1 = Tema(nombre=u'Cómo funciona esta mierda?????', foro=f1)
-    m1 = Mensaje(autor=u1, tema=t1, texto='Hola no funciona. Por favor ayudenme!!!')
+print('\n\n==== Using ClassDiagram1.xmi (transformed by the m2m class2table transformation)')
+u1 = Usuario(nombre='yo')
+f1 = Foro(nombre='Preguntas generales')
+t1 = Tema(nombre=u'Cómo funciona esta mierda?????', foro=f1)
+m1 = Mensaje(autor=u1, tema=t1, texto='Hola no funciona. Por favor ayudenme!!!')
 
-    print('m1 =', m1)
-    print('tema =', m1.tema.nombre)
-    print('foro =', m1.tema.foro.nombre)
-    print('autor =', m1.autor.nombre)
-    assert m1.tema.foro == f1
+print('m1 =', m1)
+print('tema =', m1.tema.nombre)
+print('foro =', m1.tema.foro.nombre)
+print('autor =', m1.autor.nombre)
+assert m1.tema.foro == f1
 
-    session = setup()
-    session.add_all([u1, f1, t1, m1])
-    session.commit()
+print('\n== Setting up database')
 
-    print('u1.id =', u1.id, '==', 'm1.autor_id =', m1.autor_id)
-    print('t1.pk =', t1.pk, '==', 'm1.tema_pk =', m1.tema_pk)
+engine = create_engine('sqlite:///:memory:')#, echo=True)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+session.add_all([u1, f1, t1, m1])
+session.commit()
 
-    print('u1 from database:', session.query(Usuario).filter(Usuario.nombre == 'yo').one().id)
+print('u1.id =', u1.id, '==', 'm1.autor_id =', m1.autor_id)
+print('t1.pk =', t1.pk, '==', 'm1.tema_pk =', m1.tema_pk)
 
-
-def main(args):
-    cmds = {n[3:]: f for n,f in globals().items() if n.startswith('do_')}
-    if not args or args[0] not in cmds:
-        print('Available commands:')
-        for c in cmds.keys():
-            print('  %s' % c)
-        return
-
-    cmds[args[0]]()
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
+print('u1 from database:', session.query(Usuario).filter(Usuario.nombre == 'yo').one().id)
